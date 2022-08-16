c829c302a11aa8aa83f8fd497c2c14d66e64944d
spidΞy <web3slinger@gmail.com>
2022-08-16 16:25:48 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  const test = 'test'
+  return (
+    <>
+      <Head>
+        <title>Supabase Test</title>
+        <meta name="description" content="Supabase Test" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
